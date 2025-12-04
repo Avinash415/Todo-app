cd6638eee9680136752b846ec472c083eeef7fa7
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2025-12-04 14:49:03 +0530

    Add: Some new lines of code

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Todo from '../components/Todo';
 export default function Page() {
   return (
     <main style={{padding:20, fontFamily:'system-ui, sans-serif'}}>
-      <h1>Todo (Next.js)</h1>
+      <h1>Todo Functional App(Next.js)</h1>
       <Todo />
     </main>
   );
